54ea94d2851e0b873818767e5c012984218bb929
sungyeon-0975 <sustain98@naver.com>
2021-03-31 15:38:39 +0900

    로그인 UI

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,5 +1,65 @@
 <template>
-  <div class="about">
-    <h1>Login</h1>
-  </div>
+  <v-card
+    class="pa-auto ma-auto px-25 rounded-xl"
+    width="500px"
+    outlined>
+    <v-card-title
+      class="justify-center">
+      Login
+    </v-card-title>
+    <v-card-actions 
+      class="justify-center"
+
+    >
+    <v-form
+      ref="form"
+      v-model="valid"
+      lazy-validation
+    >
+      <v-text-field
+        solo
+        dense
+        v-model="id"
+        label="ID"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        solo
+        dense
+        v-model="password"
+        label="PW"
+        required
+      ></v-text-field>
+
+      <v-btn
+        :disabled="!valid"
+        color="primary"
+        class="mr-4"
+        @click="validate"
+      >
+        Login
+      </v-btn>
+    </v-form>
+  </v-card-actions>
+ </v-card>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      valid: true,
+      id: '',
+      password: '',
+    }),
+
+    methods: {
+      validate () {
+        this.$refs.form.validate()
+      },
+    },
+  }
+</script>
+<style scoped>
+
+</style>
